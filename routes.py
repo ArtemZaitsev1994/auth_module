07b2aea08573eafd3a42c6517fa8732e63991f0d
ArtemZaitsev1994 <38044240+ArtemZaitsev1994@users.noreply.github.com>
2020-06-18 19:48:18 +0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -5,10 +5,10 @@ from auth.api import create_user, check_token
 routes = [
     ('GET', '/', get_login_page, 'login_page'),
     ('POST', '/login', login, 'login'),
-    ('POST', '/login/change_password', change_password, 'change_password'),
 
     ('GET', '/create_user', get_create_user_page, 'create_user_page'),
 
     ('POST', '/api/create_user', create_user, 'create_user'),
     ('POST', '/api/check_token', check_token, 'check_token'),
+    ('POST', '/api/change_password', change_password, 'change_password'),
 ]
